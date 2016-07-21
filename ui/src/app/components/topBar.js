8d4cf858730012b8d4c241fe3dfc90e23aea0b90
ridakk <kadirgoktas@gmail.com>
2016-07-22 00:08:24 +0300

    eslint fix for topBar.js

--- ui/src/app/components/topBar.js
+++ ui/src/app/components/topBar.js
@@ -4,25 +4,22 @@ import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import Divider from 'material-ui/Divider';
 import { withRouter } from 'react-router';
 import session from '../models/session';
 
-let ROLES = require('../../../../constants/roles.js');
+const ROLES = require('../../../../constants/roles.js');
 
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      admin: session.get('role') === ROLES.ADMIN
+      admin: session.get('role') === ROLES.ADMIN,
     };
     this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
   }
 
   handleMenuItemClick(event, child) {
-    console.log('child: ', child);
-    console.log('path: ', child.props.path);
     if (child.props.path) {
       this.props.router.push(child.props.path);
     } else {
@@ -41,24 +38,29 @@ class TopBar extends React.Component {
             iconButtonElement={
               <IconButton><MoreVertIcon /></IconButton>
             }
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-            menuStyle={{width: '%100'}}
+            targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+            menuStyle={{ width: '%100' }}
             onItemTouchTap={this.handleMenuItemClick}
           >
-            <MenuItem primaryText="My Apps" path={'/myApps'}/>
-            {this.state.admin && <MenuItem primaryText="Apps" path={'/apps'}/>}
-            {this.state.admin && <MenuItem primaryText="Users" path={'/users'}/>}
+            <MenuItem primaryText="My Apps" path={'/myApps'} />
+            {this.state.admin && <MenuItem primaryText="Apps" path={'/apps'} />}
+            {this.state.admin && <MenuItem primaryText="Users" path={'/users'} />}
             {this.state.admin && <Divider />}
-            {this.state.admin && <MenuItem primaryText="Create New Application" path={'/newApp'}/>}
-            {this.state.admin && <MenuItem primaryText="Create New User" path={'/newUser'}/>}
+            {this.state.admin && <MenuItem primaryText="Create New Application" path={'/newApp'} />}
+            {this.state.admin && <MenuItem primaryText="Create New User" path={'/newUser'} />}
             <Divider />
-            <MenuItem primaryText="Log out"/>
+            <MenuItem primaryText="Log out" />
           </IconMenu>
         }
       />
     );
   }
 }
+
+TopBar.propTypes = {
+  router: React.PropTypes.func.isRequired,
+  title: React.PropTypes.string.isRequired,
+};
 
 export default withRouter(TopBar);
