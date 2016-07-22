e841cc003adefc827cc483c410b787b8b498016a
ridakk <kadirgoktas@gmail.com>
2016-07-22 12:20:24 +0300

    eslint fix for api.js

--- ui/src/app/services/api.js
+++ ui/src/app/services/api.js
@@ -1,44 +1,44 @@
 import session from '../models/session';
 
-let Api = () => {
+const Api = () => {
   function sendRequest(url, method, headers, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
-          method: method,
-          headers: headers,
-          body: JSON.stringify(data)
-        }).then((response) => {
-          console.log(url + ' ' + method + ' response: ', response);
-          if (!response.ok) {
-            reject({
-              errorCode: response.status,
-              reasonText: response.statusText
-            });
-            return;
-          }
-          resolve(response.json());
-        })
-        .catch((err) => {
-          reject(err);
-        });
+        method,
+        headers,
+        body: JSON.stringify(data),
+      }).then((response) => {
+        if (!response.ok) {
+          reject({
+            errorCode: response.status,
+            reasonText: response.statusText,
+          });
+          return;
+        }
+        resolve(response.json());
+      })
+      .catch((err) => {
+        reject(err);
+      });
     });
   }
 
   return {
     auth: (username, password) => {
+      const auth = btoa(`${username}:${password}`);
+
       return sendRequest('/auth', 'GET', new Headers({
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'Basic ' + btoa(username + ':' + password)
+        Authorization: `Basic ${auth}`,
       }));
     },
-    send: (url, method, data) => {
-      return sendRequest(url, method, new Headers({
-        'Accept': 'application/json',
+    send: (url, method, data) =>
+      sendRequest(url, method, new Headers({
+        Accept: 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': 'JWT ' + session.get('token')
-      }), data);
-    }
+        Authorization: `JWT ${session.get('token')}`,
+      }), data),
   };
 };
 
