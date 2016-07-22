06d08881ac10481075895e7d0459af1f58da8aa7
ridakk <kadirgoktas@gmail.com>
2016-07-22 13:21:04 +0300

    eslint fix for components/apps.js

--- ui/src/app/components/apps.js
+++ ui/src/app/components/apps.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import {deepOrange500} from 'material-ui/styles/colors';
+import { deepOrange500 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TopBar from './topBar';
 import DataList from './dataList';
 import session from '../models/session';
 import api from '../services/api';
-import {AppRightMenu} from '../constants/rightMenu';
-import {folderIcon} from '../constants/icons';
+import { AppRightMenu } from '../constants/rightMenu';
+import { folderIcon } from '../constants/icons';
 import { withRouter } from 'react-router';
 
 const styles = {
   container: {
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 };
 
 const muiTheme = getMuiTheme({
   palette: {
-    accent1Color: deepOrange500
-  }
+    accent1Color: deepOrange500,
+  },
 });
 
 class Apps extends React.Component {
@@ -28,18 +28,16 @@ class Apps extends React.Component {
     this.handleRightIconMenuClick = this.handleRightIconMenuClick.bind(this);
     this.state = {
       applications: [],
-      selectedApp: null
+      selectedApp: null,
     };
-    console.log('home page state: ', this.state);
-    api.send('/applications', 'GET').then((applications)=>{
+    api.send('/applications', 'GET').then((applications) => {
       this.setState({
-        applications: applications
+        applications,
       });
     });
   }
 
   handleRightIconMenuClick(event, child) {
-    console.log('selected app id', child.props.id);
     session.set('selectedApp', child.props.id);
     this.props.router.push(child.props.path);
   }
@@ -48,20 +46,28 @@ class Apps extends React.Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={styles.container}>
-          <TopBar title={'Apps'}/>
-          {this.state.applications.length > 0 && <DataList
-            content={this.state.applications}
-            idKey={'id'}
-            primaryTextKey={'name'}
-            secondaryTextKey={'url'}
-            rightIconMenu={AppRightMenu}
-            rightIconMenuClick={this.handleRightIconMenuClick}
-            leftAvatar={folderIcon}/>}
-          {this.state.applications.length === 0 && <h3>Please create new application from left menu</h3>}
+          <TopBar title={'Apps'} />
+          {this.state.applications.length > 0 &&
+            <DataList
+              content={this.state.applications}
+              idKey={'id'}
+              primaryTextKey={'name'}
+              secondaryTextKey={'url'}
+              rightIconMenu={AppRightMenu}
+              rightIconMenuClick={this.handleRightIconMenuClick}
+              leftAvatar={folderIcon}
+            />
+          }
+          {this.state.applications.length === 0 &&
+            <h3>Please create new application from left menu</h3>}
         </div>
       </MuiThemeProvider>
     );
   }
 }
 
-export default  withRouter(Apps);
+Apps.propTypes = {
+  router: React.PropTypes.func.isRequired,
+};
+
+export default withRouter(Apps);
