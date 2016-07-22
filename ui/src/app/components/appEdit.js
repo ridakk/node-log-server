89aae068e15026d03f7cdb0a18c1950c53ea3068
ridakk <kadirgoktas@gmail.com>
2016-07-22 13:31:13 +0300

    eslint fix for appEdit.js

--- ui/src/app/components/appEdit.js
+++ ui/src/app/components/appEdit.js
@@ -1,13 +1,11 @@
 
 import React from 'react';
-import {deepOrange500} from 'material-ui/styles/colors';
+import { deepOrange500 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TopBar from './topBar';
 import session from '../models/session';
 import api from '../services/api';
-import {AppRightMenu} from '../constants/rightMenu';
-import {folderIcon} from '../constants/icons';
 import ChipList from './chipList';
 import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
@@ -19,14 +17,14 @@ import FlatButton from 'material-ui/FlatButton';
 
 const styles = {
   container: {
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 };
 
 const muiTheme = getMuiTheme({
   palette: {
-    accent1Color: deepOrange500
-  }
+    accent1Color: deepOrange500,
+  },
 });
 
 class AppEdit extends React.Component {
@@ -48,67 +46,64 @@ class AppEdit extends React.Component {
       userToAdd: '',
       notificationOpen: false,
       notificationMessage: '',
-      appId: session.get('selectedApp')
+      appId: session.get('selectedApp'),
     };
-    console.log('app edit state: ', this.state);
-    api.send('/application/' + this.state.appId, 'GET').then((application)=>{
+    api.send(`/application/${this.state.appId}`, 'GET').then((application) => {
       this.setState({
-        application: application
+        application,
       });
     });
-    api.send('/key/' + this.state.appId, 'GET').then((keys)=>{
+    api.send(`/key/${this.state.appId}`, 'GET').then((keys) => {
       this.setState({
-        keys: keys
+        keys,
       });
     });
-    api.send('/users', 'GET').then((users)=>{
+    api.send('/users', 'GET').then((users) => {
       this.setState({
-        userDataSource: users
+        userDataSource: users,
       });
     });
-    api.send('/user/applications/' + this.state.appId, 'GET').then((users)=>{
+    api.send(`/user/applications/${this.state.appId}`, 'GET').then((users) => {
       this.setState({
-        users: users
+        users,
       });
     });
   }
 
   handleGenerateNewKey() {
-    console.log('key create clicked');
-    api.send('/key/' + this.state.appId, 'POST').then((newKey) => {
-      let keys = this.state.keys;
+    api.send(`/key/${this.state.appId}`, 'POST').then((newKey) => {
+      const keys = this.state.keys;
 
       keys.push(newKey);
       this.setState({
-        keys: keys,
+        keys,
         notificationOpen: false,
         notificationMessage: '',
         newKeyDialog: true,
-        newKey: newKey
+        newKey,
       });
     }, (err) => {
       this.setState({
         notificationOpen: true,
-        notificationMessage: 'Failed to generate key: ' + err.reasonText
+        notificationMessage: `Failed to generate key: ${err.reasonText}`,
       });
     });
   }
 
   handleKeyDeleteRequest(keyId) {
-    console.log('key delete clicked');
-    api.send('/key/' + keyId, 'DELETE').then(() => {
-      let keys = this.state.keys;
+    api.send(`/key/${keyId}`, 'DELETE').then(() => {
+      const keys = this.state.keys;
 
       keys.splice(keys.indexOf(keys.find(key => key.id === keyId)), 1);
       this.setState({
-        keys: keys,
+        keys,
         notificationOpen: false,
-        notificationMessage: ''
+        notificationMessage: '',
       });
     }, (err) => {
       this.setState({
         notificationOpen: true,
-        notificationMessage: 'Failed to remove key: ' + err.reasonText
+        notificationMessage: `Failed to remove key: ${err.reasonText}`,
       });
     });
   }
@@ -118,46 +113,44 @@ class AppEdit extends React.Component {
       notificationOpen: false,
       notificationMessage: '',
       newKeyDialog: false,
-      newKey: {}
+      newKey: {},
     });
   }
 
   handleAddUser() {
-    console.log('add user clicked');
-    api.send('/user/' + this.state.userToAdd.username + '/' + this.state.appId, 'POST').then(() => {
-      let users = this.state.users;
+    api.send(`/user/${this.state.userToAdd.username}/${this.state.appId}`, 'POST').then(() => {
+      const users = this.state.users;
 
       users.push({
-        username: this.state.userToAdd.username
+        username: this.state.userToAdd.username,
       });
       this.setState({
-        users: users,
+        users,
         notificationOpen: false,
-        notificationMessage: ''
+        notificationMessage: '',
       });
     }, (err) => {
       this.setState({
         notificationOpen: true,
-        notificationMessage: 'Failed to add user: ' + err.reasonText
+        notificationMessage: `Failed to add user: ${err.reasonText}`,
       });
     });
   }
 
   handleUserDeleteRequest(username) {
-    console.log('remove user clicked: ' + username);
-    api.send('/user/' + username + '/' + this.state.appId, 'DELETE').then(() => {
-      let users = this.state.users;
+    api.send(`/user/${username}/${this.state.appId}`, 'DELETE').then(() => {
+      const users = this.state.users;
 
       users.splice(users.indexOf(users.find(user => user.username === username)), 1);
       this.setState({
-        users: users,
+        users,
         notificationOpen: false,
-        notificationMessage: ''
+        notificationMessage: '',
       });
     }, (err) => {
       this.setState({
         notificationOpen: true,
-        notificationMessage: 'Failed to remove user: ' + err.reasonText
+        notificationMessage: `Failed to remove user: ${err.reasonText}`,
       });
     });
   }
@@ -166,7 +159,7 @@ class AppEdit extends React.Component {
     this.setState({
       userToAdd: value,
       notificationOpen: false,
-      notificationMessage: ''
+      notificationMessage: '',
     });
   }
 
@@ -174,7 +167,7 @@ class AppEdit extends React.Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div style={styles.container}>
-          <TopBar title={this.state.application.name}/>
+          <TopBar title={this.state.application.name} />
           <h4>{this.state.application.url}</h4>
           <h4>{this.state.appId}</h4>
           <Subheader>Keys</Subheader>
@@ -182,56 +175,58 @@ class AppEdit extends React.Component {
             content={this.state.keys}
             idKey={'id'}
             labelKey={'id'}
-            onRequestDelete={this.handleKeyDeleteRequest}/>
-            <Button
-              label={'Generate new key'}
-              disabled={false}
-              onClick={this.handleGenerateNewKey}
-            />
+            onRequestDelete={this.handleKeyDeleteRequest}
+          />
+          <Button
+            label={'Generate new key'}
+            disabled={false}
+            onClick={this.handleGenerateNewKey}
+          />
           <Divider />
           <Subheader>Users</Subheader>
           <ChipList
             content={this.state.users}
             idKey={'username'}
             labelKey={'username'}
-            onRequestDelete={this.handleUserDeleteRequest}/>
-            <AutoComplete
-              hintText="Type username"
-              dataSource={this.state.userDataSource}
-              dataSourceConfig={{
-                text: 'username',
-                value: 'username'
-              }}
-              onNewRequest={this.handleUpdateInput}
-            />
-            <Button
-              label={'Add user'}
-              disabled={false}
-              onClick={this.handleAddUser}
-            />
-            <Dialog
-              title="New key is generated"
-              modal={false}
-              contentStyle={{
-                width: '100%'
-              }}
-              actions={<FlatButton
-                label="Close"
-                primary={true}
-                keyboardFocused={true}
-                onTouchTap={this.handleNewKeyDialogClose}
-              />}
-              open={this.state.newKeyDialog}
-            >
-              <h3>Please note ProductKey & JsKey!!!
-              You can not see them after closing this dialog</h3>
-              <h4>ProductKey:</h4> {this.state.newKey.productKey}
-              <h4>JsKey:</h4> {this.state.newKey.jsKey}
-            </Dialog>
-            <Notification
-              open={this.state.notificationOpen}
-              message={this.state.notificationMessage}
-            />
+            onRequestDelete={this.handleUserDeleteRequest}
+          />
+          <AutoComplete
+            hintText="Type username"
+            dataSource={this.state.userDataSource}
+            dataSourceConfig={{
+              text: 'username',
+              value: 'username',
+            }}
+            onNewRequest={this.handleUpdateInput}
+          />
+          <Button
+            label={'Add user'}
+            disabled={false}
+            onClick={this.handleAddUser}
+          />
+          <Dialog
+            title="New key is generated"
+            modal={false}
+            contentStyle={{
+              width: '100%',
+            }}
+            actions={<FlatButton
+              label="Close"
+              primary
+              keyboardFocused
+              onTouchTap={this.handleNewKeyDialogClose}
+            />}
+            open={this.state.newKeyDialog}
+          >
+            <h3>Please note ProductKey & JsKey!!!
+            You can not see them after closing this dialog</h3>
+            <h4>ProductKey:</h4> {this.state.newKey.productKey}
+            <h4>JsKey:</h4> {this.state.newKey.jsKey}
+          </Dialog>
+          <Notification
+            open={this.state.notificationOpen}
+            message={this.state.notificationMessage}
+          />
         </div>
       </MuiThemeProvider>
     );
